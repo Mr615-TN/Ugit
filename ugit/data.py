c9b2ecffaecb8254e53b89dd01fe7c2c4c18ceb3
Tanish Neema <tneema501@gmail.com>
2025-10-21 21:14:46 -0500

    Update

---
+++ ugit/data.py
@@ -0,0 +1,45 @@
+import hashlib
+import os
+
+GIT_DIR = '.ugit'
+
+def init():
+    os.makedirs (GIT_DIR, exist_ok=True)
+    os.makedirs (f'{GIT_DIR}/objects', exist_ok=True)
+
+def update_ref(ref, oid):
+    ref_path = f'{GIT_DIR}/{ref}'
+    os.makedirs(os.path.dirname(ref_path), exist_ok=True)
+    with open(f'{GIT_DIR}/{ref}', 'w') as f:
+        f.write(oid)
+
+def get_ref(ref):
+    ref_path = f'{GIT_DIR}/{ref}'
+    if os.path.isfile(ref_path):
+        with open(ref_path) as f:
+            return f.read().strip()
+
+def iter_refs():
+    refs = ['HEAD']
+    for root, _, filenames in os.walk(f'{GIT_DIR}/refs/'):
+        root = os.path.relpath(root, GIT_DIR)
+        refs.extend(f'{root}/{name}' for name in filenames)
+    for refname in refs:
+        yield refname, get_ref(refname)
+
+
+def hash_object(data, type_='blob'):
+    obj = type_.encode() + b'\x00' + data
+    oid = hashlib.sha1(obj).hexdigest()
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(obj)
+    return oid
+
+def get_object(oid, expected='blob'):
+    with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        obj f.read()
+    type_, _, content = obj.partition(b'\x00')
+    type_ = type_.decode()
+    if expected is not None:
+        assert type_ == expected, f'expected {expected}, got {type_}'
+    return content
